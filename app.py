5b91a87dfbf188edb889fd79b12cc1e57067e927
YukiTakada <yuki.takada.3112@gmail.com>
2021-09-19 23:39:14 +0900

    app.pyを変更
    - 型ヒントを追加
    - ページ表示メソッド名変更
    - ページに表示される文言を変更

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
+app: Flask = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello !</p>"
+def show_verification_page() -> str:
+    return "<p>ローカル開発環境起動確認ページ</p>"
